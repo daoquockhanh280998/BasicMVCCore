1787da809395a23571762894d024e8dc26885b51
daoquockhanh280998 <daoquockhanh280998@gmail.com>
2020-09-30 20:21:41 +0700

    update 30/09/2020

---
+++ BasicMvcCore3.n/BasicMvcCore3.n/Controllers/ProductController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BasicMvcCore3.n.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicMvcCore3.n.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ProductEditModel model)
+        {
+            string message = "";
+
+            if (ModelState.IsValid)
+            {
+                ////check product name exist
+                //if (model.Name=="test")
+                //{
+                //    ModelState.AddModelError("", "this product name was existed");
+                //    return View(model);
+                //}
+                // check name name exist
+               
+                message = "product " + model.Name + " Rate " + model.Rate.ToString() + " With Rating " + model.Rating.ToString() + " created successfully";
+            }
+            else
+            {
+                return View(model);
+            }
+            return Content(message);
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(ProductEditModel model)
+        {
+            string message = "";
+
+            if (ModelState.IsValid)
+            {
+                message = "product " + model.Name + " Rate " + model.Rate.ToString() + " With Rating " + model.Rating.ToString() + " created successfully";
+            }
+            else
+            {
+                message = "Failed to create the product. Please try again";
+            }
+            return Content(message);
+        }
+    }
+}
---
+++ BasicMvcCore3.n/BasicMvcCore3.n/Models/ProductEditModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicMvcCore3.n.Models
+{
+    public class ProductEditModel
+    {
+  
+        public int ID { get; set; }
+        [Required(ErrorMessage ="Name lỗi")]
+        [StringLength(maximumLength:25,MinimumLength =3,ErrorMessage ="3 to 25")]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        public string EmailID { get; set; }
+        public decimal Rate { get; set; }
+        public int Rating { get; set; }
+        public DateTime DateOfJoining { get; set; }
+    }
+}
